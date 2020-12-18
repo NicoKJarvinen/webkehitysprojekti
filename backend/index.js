84ac7336e949a9ecafaae5d037f18b3b71a99619
Nico Järvinen <nico.k.jarvinen@gmail.com>
2020-12-18 03:37:43 +0200

    make it better

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,11 @@
-const app = require('./app')
-const http = require('http')
-const config = require('./utils/config')
-const logger = require('./utils/logger')
+const app = require("./app");
+const http = require("http");
+const config = require("./utils/config");
+const logger = require("./utils/logger");
 
-const server = http.createServer(app)
+const server = http.createServer(app);
 
-server.listen(config.PORT, () => {
-  logger.info(`Server running on port ${config.PORT}`)
-})
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
